c1319989d794a7191e3a9d6f185f807f5e6bd30a
Iggibob <76250539+Iggibob@users.noreply.github.com>
2021-01-19 19:59:31 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -28,13 +28,26 @@ window.addEventListener('DOMContentLoaded', function () {
     document.getElementById('burgerMenu').style.transform = 'translateX(-101%)';
 	});
 	
-	// Header search-menu
-	document.querySelector('.search__btn').addEventListener('click', function () {
-    document.getElementById('headerSearchMenu').style.transform = 'translateY(0%)';
-  });
-  document.querySelector('.search-close').addEventListener('click', function () {
-    document.getElementById('headerSearchMenu').style.transform = 'translateY(-101%)';
-	});
-	
-
 });
+
+
+/* When the user clicks on the button, 
+		toggle between hiding and showing the Header search-menu */
+		function myFunction() {
+			document.getElementById("headerSearchMenu").classList.toggle("search-show");
+	}
+	
+	// Close the Header search-menu if the user clicks outside of it
+	window.onclick = function(event) {
+	if (!event.target.matches('.search__btn')) {
+
+		var dropdowns = document.getElementsByClassName("search__block");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('search-show')) {
+				openDropdown.classList.remove('search-show');
+			}
+		}
+	}
+}
