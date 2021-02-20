e4db7ad25dab04337258390482a6c81b0631a711
Iggibob <76250539+Iggibob@users.noreply.github.com>
2021-02-20 14:51:14 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -37,5 +37,77 @@ window.addEventListener('DOMContentLoaded', function () {
 	document.querySelector('.search-close').addEventListener('click', function () {
     document.getElementById("headerSearchMenu").classList.remove("show");
   });
+
+
+	// Gallery Choices
+	const element = document.querySelector('#gallerySelectCustom');
+    const choices = new Choices(element, {
+      searchEnabled: false,
+
+			itemSelectText: 'Нажмите для выбора',
+				
+  	});
+
+	
+
+	// Gallery Slider
+	const swiper = new Swiper('.swiper-container', {
+		// Default parameters
+		slidesPerView: 1,
+		spaceBetween: 30,
+		initialSlide: 14,
+		speed: 400,
+		// Responsive breakpoints
+		breakpoints: {
+			// when window width is >= 768px
+			768: {
+				initialSlide: 4,
+				spaceBetween: 34,
+
+				slidesPerView: 2,/*сколько колонок*/
+				slidesPerColumn: 2, /*сколько столбцов*/
+				slidesPerGroup: 2, /*на сколько передвигаются*/
+				
+				speed: 600,
+			},
+			// when window width is >= 1400px
+			1400: {
+				initialSlide: 5,
+				spaceBetween: 34,
+
+				slidesPerView: 3,/*сколько колонок*/
+				slidesPerColumn: 2, /*сколько столбцов*/
+				slidesPerGroup: 3, /*на сколько передвигаются*/
+				
+				speed: 1000,
+			},
+			// when window width is >= 1700px
+			1700: {
+				initialSlide: 5,
+				spaceBetween: 50,
+
+				slidesPerView: 3,/*сколько колонок*/
+				slidesPerColumn: 2, /*сколько столбцов*/
+				slidesPerGroup: 3, /*на сколько передвигаются*/
+				
+				speed: 1000,
+			}
+		},
+		
+		// Optional parameters
+		direction: 'horizontal',
+		loop: true,
+		// Pagination arrows
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'fraction',
+		},
+		// Navigation arrows
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+
+	});
 	
 });
